f3b138b99d165eb278c76ca71d512bab472a0cfd
Olesia Lebedeva <olesia.lebedeva@cadexchanger.com>
2025-05-14 21:47:06 +0300

    feat: add brain-calc game implementation

---
+++ bin/brain-calc.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { runGame } from '../src/index.js';
+import { generateRound } from '../src/games/calc.js';
+
+const description = 'What is the result of the expression?';
+
+runGame(description, generateRound);
