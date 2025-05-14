240fcb60857d35b5d21a7bbe1069290e9db7bd81
Olesia Lebedeva <olesia.lebedeva@cadexchanger.com>
2025-05-14 22:17:52 +0300

    feat: implement brain-gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { runGame } from '../src/index.js';
+import { generateRound } from '../src/games/gcd.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+runGame(description, generateRound);
---
+++ src/games/gcd.js
@@ -0,0 +1,14 @@
+const generateRound = () => {
+  const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  
+  const findGCD = (a, b) => (b === 0 ? a : findGCD(b, a % b));
+
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(findGCD(num1, num2));
+
+  return [question, correctAnswer];
+};
+
+export { generateRound };
