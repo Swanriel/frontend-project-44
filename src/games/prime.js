702d186ea9f685bd79bacf87c97d33718dcc23f1
Olesia Lebedeva <olesia.lebedeva@cadexchanger.com>
2025-06-21 03:46:43 +0300

    Updated linter

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,13 +7,13 @@ const isPrime = (num) => {
     if (num % i === 0) return false
   }
   return true
-};
+}
 
 const generateRound = () => {
   const number = getRandomNumber(1, 100)
   const correctAnswer = isPrime(number) ? 'yes' : 'no'
   return [String(number), correctAnswer]
-};
+}
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
